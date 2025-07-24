c896d8a51d7ac192147bf97f5a2496776c1e0288
GDefalt <bluegamer2627@hotmail.com>
2025-07-24 14:12:19 -0300

    Product entity created and many-to-many association made

---
+++ src/main/java/com/firstproject/course/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.firstproject.course.repositories;
+
+import com.firstproject.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
